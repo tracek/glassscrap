84146f1b390b4a3a8c60167f725141e313236c8f
tracek <lukasz.tracewski@gmail.com>
2018-01-01 09:39:53 +0100

    refactor local crawler to match web crawler

--- localcrawler.py
+++ localcrawler.py
@@ -1,20 +1,21 @@
 import glob
 
+
 class LocalCrawler(object):
 
     def __init__(self, path):
         self.pages_visited = 0
         self.path = path
         self.pages = glob.glob(path + '/*.htm')
-        self.total_pages = self.get_total_pages()
-
-    def get_total_pages(self):
-        return len(self.pages)
+        if not self.pages:
+            raise FileNotFoundError('No files found on the path: {}'.format(path))
+        self.total_pages = len(self.pages)
 
     def get_page(self):
         for page in self.pages:
             with open(page, 'r') as f:
-               yield f.read()
+                self.pages_visited += 1
+                yield f.read()
 
 
 if __name__ == '__main__':
